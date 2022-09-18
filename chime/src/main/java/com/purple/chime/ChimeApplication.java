15482ec7a21964813aca1f59fbee24309f33cb19
vyomaaverse <nandita.aka.vyomaa@gmail.com>
2022-09-19 01:12:14 +0530

    Initial commit

---
+++ chime/src/main/java/com/purple/chime/ChimeApplication.java
@@ -0,0 +1,13 @@
+package com.purple.chime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChimeApplication.class, args);
+	}
+
+}
---
+++ chime/src/test/java/com/purple/chime/ChimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purple.chime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
